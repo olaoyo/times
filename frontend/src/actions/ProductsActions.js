5418554317f672f3872d92a00407303b1fb00747
Ola Oyo <ola.oyo@gmail.com>
2022-09-21 15:28:32 +0100

    Product list reducer and action

---
+++ frontend/src/actions/ProductsActions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+import {
+  PRODUCTS_LIST_REQUEST,
+  PRODUCTS_LIST_SUCCESS,
+  PRODUCTS_LIST_FAIL,
+} from "../constants/productsConstants";
+
+const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCTS_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products/");
+
+    dispatch({
+      type: PRODUCTS_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCTS_LIST_FAIL,
+      payload:
+        error.response && error.response.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
---
+++ frontend/src/reducers/productsReducers.js
@@ -0,0 +1,21 @@
+import {
+  PRODUCTS_LIST_REQUEST,
+  PRODUCTS_LIST_SUCCESS,
+  PRODUCTS_LIST_FAIL,
+} from "../constants/productsConstants";
+
+export const productsListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCTS_LIST_REQUEST:
+      return { loading: true, products: [] };
+
+    case PRODUCTS_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+
+    case PRODUCTS_LIST_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
