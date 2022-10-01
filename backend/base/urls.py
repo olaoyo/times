c5cc6404dd537825c990b1b312df0faec70bcec3
Ola Oyo <ola.oyo@gmail.com>
2022-10-01 11:59:15 +0100

    User serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path("", views.getRoutes, name="routes"),
+
+    path("users/profile/", views.getUserProfile, name="users-profile"),
+
     path("products/", views.getProducts, name="products"),
     path("products/<str:watch_id>/", views.getProduct, name="product"),
 ]
