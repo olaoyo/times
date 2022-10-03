75d7889d0aad40699ab3a38ca914a6e5933591c1
Ola Oyo <ola.oyo@gmail.com>
2022-10-03 11:07:26 +0100

    URLs & views cleanup

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
-
-from . models import Product
-from . products import products
-from . serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
+
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
      def validate(self, attrs):
         data = super().validate(attrs)
@@ -65,15 +63,3 @@ def getUsers(request):
     return Response(serializer.data)
 
 
-@api_view(["GET"])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getProduct(request, watch_id):
-    product = Product.objects.get(_id=watch_id)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
