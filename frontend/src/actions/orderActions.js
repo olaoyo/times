195929bb33245ae290e3cbd10a48a24400c8a0fe
Ola Oyo <ola.oyo@gmail.com>
2022-10-13 10:58:19 +0100

    Order details reducers & actions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,11 @@ import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
+
+    
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
   } from "../constants/orderConstants";
 
 import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
@@ -43,6 +48,42 @@ import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
     } catch (error) {
       dispatch({
         type: ORDER_CREATE_FAIL,
+        payload:
+          error.response && error.response.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
+
+
+  export const getOrderDetails = (orderId) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_DETAILS_REQUEST,
+      });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`
+        },
+      };
+  
+      const { data } = await axios.get(`api/orders/${orderId}/`, config);
+  
+      dispatch({
+        type: ORDER_DETAILS_SUCCESS,
+        payload: data,
+      });
+        
+    } catch (error) {
+      dispatch({
+        type: ORDER_DETAILS_FAIL,
         payload:
           error.response && error.response.detail
             ? error.response.data.detail
