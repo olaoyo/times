419dbb3d579549935aa1f5ea514325cbd97c7b5c
Ola Oyo <ola.oyo@gmail.com>
2022-10-20 12:11:22 +0100

    Get user by ID & update user endpoints

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path("profile/update/", views.updateUserProfile, name="user-profile-update"),
     path("", views.getUsers, name="users"),
 
+    path("<str:user_id>/", views.getUserById, name="user"),
+
+    path("update/<str:user_id>/", views.updateUser, name="user-update"),
+
     path("delete/<str:user_id>/", views.deleteUser, name="user-delete"),
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -85,6 +85,33 @@ def getUsers(request):
     return Response(serializer.data)
 
 
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getUserById(request, user_id):
+    user = User.objects.get(id=user_id)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateUser(request, user_id):
+    user = User.objects.get(id=user_id)    
+
+    data = request.data
+
+    user.first_name = data["name"]
+    user.username = data["email"]
+    user.email = data["email"]
+    user.is_staff = data["isAdmin"]
+
+    user.save()
+    
+    serializer = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
+
+
 @api_view(["DELETE"])
 @permission_classes([IsAdminUser])
 def deleteUser(request, user_id ):
